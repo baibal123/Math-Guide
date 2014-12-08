e24f62ce7a337a940423dfb525b6ff7815d416b5
baibal123 <s7070040@kmitl.ac.th>
2014-12-09 03:41:03 +0700

    update Interface menubar and other

--- Math-Guide.py
+++ Math-Guide.py
@@ -3,35 +3,73 @@
 import sys
 from Tkinter import *
 import tkMessageBox
+def homepagedef():
+    global homepage
+    homepage = Tk()
+    homepage.geometry('450x350')
+    photo = PhotoImage(file="Teammy.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Button1 = Button(text ="Chapter 1 - Set", command = chapter1 , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =1, column =0, sticky = W)
+    Button2 = Button(text ="Chapter 2 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =2, column =0, sticky = W)
+    Button3 = Button(text ="Chapter 3 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =3, column =0, sticky = W)
+    Button4 = Button(text ="Chapter 4 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =4, column =0, sticky = W)
+    Button5 = Button(text ="Chapter 5 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =5, column =0, sticky = W)
+    texti = Label(text = 'test-->Chapter', fg = 'yellow', bg = 'black', height = 3, width = 20).grid(row =6, column =0, sticky = W)
+    Buttont1 = Button(text ="1", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 146, y = 298)
+    Buttont2 = Button(text ="2", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 191, y = 298)
+    Buttont3 = Button(text ="3", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 236, y = 298)
+    Buttont4 = Button(text ="4", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 281, y = 298)
+    Buttont5 = Button(text ="5", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 326, y = 298)
+    Buttont6 = Button(text ="Final", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 10).place(x = 371, y = 298)
 
-homepage = Tk()
+    menubar = Menu(homepage)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Score")
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command = closehomepage)
+    menubar.add_cascade(label = "File", menu = filemenu)
 
-homepage.geometry('450x350')
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="Instuction")
+    helpmenu.add_separator()
+    helpmenu.add_command(label="Credits", command = credit)
+    menubar.add_cascade(label = "Help", menu = helpmenu)
 
-photo = PhotoImage(file="Teammy.gif")
-label = Label(image=photo)
-label.image = photo
-label.grid(row =0, column =0)
-
+    homepage.config(menu=menubar)
+    homepage.mainloop()
+    
 def donothing():
     pass
 
-Button1 = Button(text ="Chapter 1 - Set", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =1, column =0, sticky = W)
-Button2 = Button(text ="Chapter 2 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =2, column =0, sticky = W)
-Button3 = Button(text ="Chapter 3 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =3, column =0, sticky = W)
-Button4 = Button(text ="Chapter 4 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =4, column =0, sticky = W)
-Button5 = Button(text ="Chapter 5 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =5, column =0, sticky = W)
+def credit():
+    tkMessageBox.showinfo("Credits", "Math-Guide V.1.0 create by baibal123(57070040)")
 
-texti = Label(text = 'test-->Chapter', fg = 'yellow', bg = 'black', height = 3, width = 20).grid(row =6, column =0, sticky = W)
+def closehomepage():
+    exits = tkMessageBox.askyesno('Exit', 'Are you sure?')
+    if exits > 0:
+        homepage.destroy()
 
-Button1 = Button(text ="1", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 146, y = 298)
-Button2 = Button(text ="2", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 191, y = 298)
-Button3 = Button(text ="3", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 236, y = 298)
-Button4 = Button(text ="4", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 281, y = 298)
-Button5 = Button(text ="5", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 326, y = 298)
-Button6 = Button(text ="Final", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 10).place(x = 371, y = 298)
+def chapter1():
+    homepage.destroy()
+    set_page = Tk()
+    global set_page
+    set_page.geometry('875x600')
+    photo = PhotoImage(file="Set_page1.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Buttont1 = Button(text ="Next", command = chapter1page2 , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
 
-homepage.mainloop()
+def chapter1page2():
+    photo = PhotoImage(file="Set_page2.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Buttont1 = Button(text ="Next", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
+
+homepagedef()
 ##import sys
 ##from Tkinter import *
 ##import tkMessageBox ##Reply massage box
