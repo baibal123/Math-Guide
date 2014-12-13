06c92a0d39db81acd71011279264864923b71a6f
baibal123 <s7070040@kmitl.ac.th>
2014-12-14 04:31:29 +0700

    Quiz1

--- Math-Guide.py
+++ Math-Guide.py
@@ -4,7 +4,6 @@ import sys
 from Tkinter import *
 import tkMessageBox
 
-
 def homepagedef():
     global homepage
     homepage = Tk()
@@ -19,7 +18,7 @@ def homepagedef():
     Button4 = Button(text ="Chapter 4 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =4, column =0, sticky = W)
     Button5 = Button(text ="Chapter 5 - propositional logic", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 63).grid(row =5, column =0, sticky = W)
     texti = Label(text = 'test-->Chapter', fg = 'yellow', bg = 'black', height = 3, width = 20).grid(row =6, column =0, sticky = W)
-    Buttont1 = Button(text ="1", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 146, y = 298)
+    Buttont1 = Button(text ="1", command = test1page1gui , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 146, y = 298)
     Buttont2 = Button(text ="2", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 191, y = 298)
     Buttont3 = Button(text ="3", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 236, y = 298)
     Buttont4 = Button(text ="4", command = donothing , fg = 'yellow', bg = 'black', height = 3, width = 5).place(x = 281, y = 298)
@@ -28,7 +27,6 @@ def homepagedef():
 
     menubar = Menu(homepage)
     filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Score")
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command = closehomepage)
     menubar.add_cascade(label = "File", menu = filemenu)
@@ -64,7 +62,6 @@ def chapter1():
     label.grid(row =0, column =0)
     menubar = Menu(set_page)
     filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Score")
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command = close_set)
     menubar.add_cascade(label = "File", menu = filemenu)
@@ -75,7 +72,6 @@ def chapter1():
     menubar.add_cascade(label = "Help", menu = helpmenu)
     set_page.config(menu=menubar)
     Buttont1 = Button(text ="Next", command = chapter1page2 , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
-
 
 def close_set():
     exits = tkMessageBox.askyesno('Exit', 'Are you sure?')
@@ -102,7 +98,7 @@ def chapter1page4():
     label.image = photo
     label.grid(row =0, column =0)
     Buttont1 = Button(text ="Next", command = chapter1page5 , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
-
+    
 def chapter1page5():
     photo = PhotoImage(file="Set_page5.gif")
     label = Label(image=photo)
@@ -122,7 +118,81 @@ def chapter1page7():
     label = Label(image=photo)
     label.image = photo
     label.grid(row =0, column =0)
-    Buttont1 = Button(text ="Next", command = donothing , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
+    Buttont1 = Button(text ="Next", command = test1page1 , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
+
+def test1page1():
+    photo = PhotoImage(file="Set_quiz1.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Buttont1 = Button(text ="1.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 165)
+    Buttont2 = Button(text ="2.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 229)
+    Buttont3 = Button(text ="3.", command = correct , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 295)
+    Buttont4 = Button(text ="4.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 364)
+
+def test1page1gui():
+    homepage.destroy()
+    global set_page
+    set_page = Tk()
+    set_page.geometry('875x600')
+    photo = PhotoImage(file="Set_quiz1.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Buttont1 = Button(text ="1.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 165)
+    Buttont2 = Button(text ="2.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 229)
+    Buttont3 = Button(text ="3.", command = correct , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 295)
+    Buttont4 = Button(text ="4.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 364)
+    menubar = Menu(set_page)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command = close_set)
+    menubar.add_cascade(label = "File", menu = filemenu)
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="Instuction")
+    helpmenu.add_separator()
+    helpmenu.add_command(label="Credits", command = credit)
+    menubar.add_cascade(label = "Help", menu = helpmenu)
+    set_page.config(menu=menubar)
+
+def correct():
+    tkMessageBox.showinfo("Answer", "Correct!!")
+    ans_page1()
+    
+def test1page2():
+    photo = PhotoImage(file="Set_quiz2.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Buttont1 = Button(text ="1.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 265)
+    Buttont2 = Button(text ="2.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 329)
+    Buttont3 = Button(text ="3.", command = correct2 , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 395)
+    Buttont4 = Button(text ="4.", command = wrong , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 540, y = 464)
+
+def correct2():
+    tkMessageBox.showinfo("Answer", "Correct!!")
+    ans_page2()
+
+def ans_page1():
+    photo = PhotoImage(file="Set_quiz1ans.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Buttont1 = Button(text ="Next", command = test1page2 , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
+
+def ans_page2():
+    photo = PhotoImage(file="Set_quiz2ans.gif")
+    label = Label(image=photo)
+    label.image = photo
+    label.grid(row =0, column =0)
+    Buttont1 = Button(text ="Next", command = back , fg = 'black', bg = 'yellow', height = 2, width = 5).place(x = 750, y = 498)
+
+def back():
+    set_page.destroy()
+    homepagedef()
+
+def wrong():
+    tkMessageBox.showinfo("Answer", "Nice Try but it's wrong!!")
 
 def chapter2():
     homepage.destroy()
@@ -135,7 +205,6 @@ def chapter2():
     label.grid(row =0, column =0)
     menubar = Menu(set_page)
     filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Score")
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command = close_set)
     menubar.add_cascade(label = "File", menu = filemenu)
@@ -291,7 +360,6 @@ def chapter3():
     label.grid(row =0, column =0)
     menubar = Menu(set_page)
     filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Score")
     filemenu.add_separator()
     filemenu.add_command(label="Exit", command = close_set)
     menubar.add_cascade(label = "File", menu = filemenu)
@@ -362,7 +430,7 @@ homepagedef()
 ##B.pack() ##For Showing the Button or another
 ##
 ##texti = Label(text = 'For testing button and entry', fg = 'white', bg = 'black').pack()
-##
+####dummy = StringVar()
 ##def test_entry():
 ##    mntry = Entry(mGui, textvariable = dummy).pack()
 ##    mbutton = Button(mGui, text = 'ok', command = showdummy).pack()
@@ -387,40 +455,6 @@ homepagedef()
 ##
 ##mGui.mainloop()
 ##________________SET____________________________________
-
-def npowset_set(setnumber):
-    """Find how many member in power set v.set"""
-    return len(setnumber)**2
-
-def npowset_n(number):
-    """Find how many member in power set v.number"""
-    return number**2
-
-def same_member(setnumber):
-    """Cutout the same member because set not count"""
-    box = []
-    for i in setnumber:
-        if i not in box:
-            box.append(i)
-    return box
-
-def list_powerset(box):
-    """Find powerset"""
-    result = [[]]
-    for i in same_member(box):
-        result.extend([subset + [i] for subset in result])
-    return result
-
-def is_subset():
-    """check subset list to list"""
-    main = input()
-    checkset = input()
-    if main == list and checkset != list:
-        return False
-    for i in checkset:
-        if i not in main:
-            return False
-    return True
 
 def is_member():
     """
